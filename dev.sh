89f1baccded2c0414a5dde6e50d8ede93a4f52e2
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-02-20 12:55:52 +0100

    fix: dev.sh config

--- dev.sh
+++ dev.sh
@@ -6,5 +6,5 @@ kubectl create secret generic gbv-bot-envs --from-env-file .env
 
 # App
 kubectl delete -f kube
-docker build -t nandanrao/gbv-botserver:0.0.2 .
+docker build -t nandanrao/gbv-botscribe:0.0.2 .
 kubectl apply -f kube
