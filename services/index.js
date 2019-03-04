9006a499c96f67cbfc5bcb2f071954d412bb472d
Nandan <nandanmarkrao@gmail.com>
2019-03-04 18:47:33 +0100

    Added chatbase-gcs, setup Keyed readstream.

--- services/index.js
+++ services/index.js
@@ -1,33 +1,52 @@
-const Kafka = require('node-rdkafka');
+const Kafka = require('node-rdkafka')
+const Chatbase = require(process.env.CHATBASE_BACKEND)
+const {KeyedStreamer, PromiseStream} = require('@vlab-research/steez')
 
-// Create a kafka consumer to retrieve messages
-const consumer = new Kafka.KafkaConsumer({
+const kafkaOpts = {
   'metadata.broker.list': `${process.env.KAFKA_BROKER}:${process.env.KAFKA_PORT}`,
   'retry.backoff.ms': 200,
   'socket.keepalive.enable': true,
   'session.timeout.ms': 60000,
-  'group.id': 'kafka' // TODO: make it dynamic to every group 
-}, {});
+  'group.id': 'botscribe',
+  'client.id': 'botscribe',
+  'enable.auto.commit': false
+}
 
-consumer.connect()
+const stream = new Kafka.createReadStream(kafkaOpts, {'auto.offset.reset': 'earliest'}, { topics: [process.env.KAFKA_TOPIC]})
+const chatbase = new Chatbase()
 
-consumer.on('event.error', err => {
-  console.error('Error from consumer:');
-  console.error(err);
-})
+const write = async (msg) => {
+  const val = msg.value.toString()
+  const key = msg.key.toString()
+  await chatbase.put(key, val)
+  stream.consumer.commitMessage(msg)
+}
 
-consumer
-  .on('ready', (err) => {
-    try {
-      consumer
-        .subscribe([ process.env.KAFKA_TOPIC ])
-        .consume();
-    } catch (err) {
-      console.log('The following error occurs subscribing a topic: ', err);
-    }
+const subStreamer = () => new PromiseStream(write, {highWaterMark: 50})
+const dbStream = new KeyedStreamer(m => m.key.toString(),
+                                        subStreamer,
+                                        {highWaterMark: 200})
+
+stream
+  .pipe(dbStream)
+  .on('error', err => {
+    console.error(err)
   })
-  .on('data', message => {
-    // Output the actual message contents
-    console.log('INCOMING MESSAGE: ', message.value.toString());
-    // TODO: add logic to write key/value messages into database
-  });
+
+
+// ------- SHUT DOWN --------
+// TODO: Cleanup!
+const signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+}
+
+for (let [signal, value] in signals) {
+  process.on(signal, () => {
+    stream.consumer.disconnect()
+    stream.consumer.on('disconnected', () => {
+      process.exit(128 + value)
+    })
+  })
+}
